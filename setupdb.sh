fdf97be709966f9f5b2a682c83b0c4c6b48e5ae9
Hiro Sato <hiro_sato@me.com>
2020-08-16 23:41:48 +0900

    Update setupdb.sh

--- setupdb.sh
+++ setupdb.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ ! -d "/run/mysqld" ]; then
-	  mkdir -p /run/mysqld
-	    chown -R mysql:mysql /run/mysqld
+  mkdir -p /run/mysqld
+  chown -R mysql:mysql /run/mysqld
 fi
 if [ ! -e /usr/my.cnf ]; then
-	  cat << EOF > /usr/my.cnf
+  cat << EOF > /usr/my.cnf
 [mysqld]
 datadir=/var/lib/mysql
 log-bin = /var/lib/mysql/mysql-bin
@@ -15,24 +15,24 @@ character-set-server = utf8
 pid-file=/run/mysqld/mysqld.pid
 EOF
   chown -R mysql:mysql /var/lib/mysql
-    mysql_install_db --user=mysql
-      nohup /bin/sh /usr/bin/mysqld_safe \
-	          --datadir=/var/lib/mysql \
-		      --socket=/run/mysqld/mysqld.sock \
-		          --pid-file=/run/mysqld/mysqld.pid \
-			      --basedir=/usr \
-			          --user=mysql \
-				      --skip-grant-tables \
-				          --skip-networking &
-        while :
-		  do
-			      [ -r /run/mysqld/mysqld.pid ] || continue
-			          if ps -ef | grep -q $(cat /run/mysqld/mysqld.pid) ; then
-					          break
-						      fi
-						          sleep 10
-							    done
-							      cat << EOF > /tmp/setupdb.sql
+  mysql_install_db --user=mysql
+  nohup /bin/sh /usr/bin/mysqld_safe \
+    --datadir=/var/lib/mysql \
+    --socket=/run/mysqld/mysqld.sock \
+    --pid-file=/run/mysqld/mysqld.pid \
+    --basedir=/usr \
+    --user=mysql \
+    --skip-grant-tables \
+    --skip-networking &
+  while :
+  do
+    [ -r /run/mysqld/mysqld.pid ] || continue
+    if ps -ef | grep -q $(cat /run/mysqld/mysqld.pid) ; then
+        break
+    fi
+    sleep 10
+  done
+  cat << EOF > /tmp/setupdb.sql
 USE mysql;
 UPDATE mysql.user SET Password=PASSWORD('piwigo') WHERE User='root';
 FLUSH PRIVILEGES;
@@ -40,11 +40,11 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'piwigo';
 FLUSH PRIVILEGES;
 EOF
   /usr/bin/mysql < /tmp/setupdb.sql
-    cat << EOF > /tmp/createdb.sql
+  cat << EOF > /tmp/createdb.sql
 CREATE DATABASE piwigo DEFAULT CHARACTER SET utf8mb4;
 EOF
   /usr/bin/mysql -uroot -ppiwigo < /tmp/createdb.sql
-    kill `cat /run/mysqld/mysqld.pid`
-      sleep 10
-				  fi
-				  exec /usr/bin/mysqld --defaults-file=/usr/my.cnf --console --user=mysql --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+  kill `cat /run/mysqld/mysqld.pid`
+  sleep 10
+fi
+exec /usr/bin/mysqld --defaults-file=/usr/my.cnf --console --user=mysql --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
